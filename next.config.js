a02d75751b045a015850670b35aeda6c17070b39
3rekse <bonfiglio.fabrizio@itiomar.net>
2025-01-03 15:27:29 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -7,7 +7,14 @@
 //module.exports = nextConfig;
 // filepath: next.config.js
 module.exports = {
-  exportTrailingSlash: true,
+  // Abilita l'aggiunta di una barra finale a tutte le URL generate
+  trailingSlash: true,
+  
+  // Imposta il prefisso degli asset in base all'ambiente di esecuzione
+  // Se l'ambiente è di produzione, utilizza '/MMXLVIII/' come prefisso
   assetPrefix: process.env.NODE_ENV === 'production' ? '/MMXLVIII/' : '',
+  
+  // Imposta il percorso base per tutte le pagine
+  // Se l'ambiente è di produzione, utilizza '/MMXLVIII' come percorso base
   basePath: process.env.NODE_ENV === 'production' ? '/MMXLVIII' : '',
 };
